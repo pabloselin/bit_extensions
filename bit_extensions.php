8223daa4c4f33e49348c3eea948a00e3ff812742
Pablo Selín Carrasco Armijo <pablo@apie.cl>
2020-01-21 16:49:10 -0300

    estructura final de base de datos

--- bit_extensions.php
+++ bit_extensions.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Extensiones Bitacora Teatro UC
  * Plugin URI:        https://example.com/plugins/the-basics/
  * Description:       Sistemas para textos y otras funcionalidades
- * Version:           0.0.1
+ * Version:           0.0.7
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Pablo Selín Carrasco Armijo 
@@ -15,47 +15,94 @@
  */
 
 
-define( 'PLUGIN_VERSION', '0.0.1' );
-define( 'DB_VERSION', '0.0.1' );
+define( 'PLUGIN_VERSION', '0.0.8' );
+define( 'BIT_DB_VERSION', '0.0.16' );
 define( 'BIT_TABLENAME', 'texto_dramatico');
+define( 'BIT_MEDIATABLENAME', 'archivo_medios');
 
 include_once( plugin_dir_path( __FILE__ ) . 'admin-interface.php');
 
 
 function bit_create_tables() {
 		global $wpdb;
-		$dbver = BIT_TABLENAME;
+		$dbver = BIT_DB_VERSION;
 		$actver = get_option('bit_dbver');
 		
 		if(!get_site_option('bit_dbver') || $dbver != get_site_option('bit_dbver')) {
 			$table_name = $wpdb->prefix . BIT_TABLENAME;
+			$mediatable_name = $wpdb->prefix . BIT_MEDIATABLENAME;
 			$charset_collate = $wpdb->get_charset_collate();
 
 			$sql = "CREATE TABLE $table_name(
-			id mediumint(9) NOT NULL AUTO_INCREMENT,
-			unidad mediumint(9) NOT NULL,
-			ids_asoc tinytext NOT NULL,
-			texto text NOT NULL,
-			tipo tinytext NOT NULL,
-			personajes text NOT NULL,
-			time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
-			data text NOT NULL,
-			confirmed boolean NOT NULL,
-			UNIQUE KEY id (id)
-		) $charset_collate;";
+						id mediumint(9) NOT NULL AUTO_INCREMENT,
+						unidad mediumint(9) NOT NULL,
+						ids_asoc tinytext NOT NULL,
+						play_asoc int NOT NULL,
+						texto text NOT NULL,
+						tipo tinytext NOT NULL,
+						personajes text NOT NULL,
+						UNIQUE KEY id (id)
+					) $charset_collate;
+					CREATE TABLE $mediatable_name(
+						id mediumint(9) NOT NULL AUTO_INCREMENT,
+						mediaid tinytext NOT NULL,
+						categoria text NOT NULL,
+						tipo_material text NOT NULL,
+						fecha_text tinytext NOT NULL,
+						descripcion_sintetica text NOT NULL,
+						descripcion_detallada text NOT NULL,
+						ingreso text NOT NULL,
+						procesamiento text NOT NULL,
+						fuente text NOT NULL,
+						play_asoc int NOT NULL,
+						UNIQUE KEY id (id)
+					) $charset_collate;";
 
 		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
 		dbDelta($sql);
-		add_option('bit_dbver', $dbver);
+		update_option('bit_dbver', $dbver);
 		}
 	}
 
-function bit_plugin_activation() {
-	//Inicializar tablas
-	bit_create_tables();
+function bit_create_mediatables() {
+		global $wpdb;
+		$dbver = BIT_DB_VERSION;
+		$actver = get_option('bit_dbver');
+		
+		if(!get_site_option('bit_dbver') || $dbver != get_site_option('bit_dbver')) {
+			$table_name = $wpdb->prefix . BIT_MEDIATABLENAME;
+			$charset_collate = $wpdb->get_charset_collate();
+
+			$sql = "CREATE TABLE $table_name(
+						id mediumint(9) NOT NULL AUTO_INCREMENT,
+						mediaid mediumint(9) NOT NULL,
+						categoria text NOT NULL,
+						tipo_material text NOT NULL,
+						fecha_text tinytext NOT NULL,
+						descripcion_sintetica text NOT NULL,
+						descripcion_detallada text NOT NULL,
+						ingreso text NOT NULL,
+						procesamiento text NOT NULL,
+						fuente text NOT NULL,
+						UNIQUE KEY id (id)
+					) $charset_collate;";
+
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		dbDelta($sql);
+		update_option('bit_dbver', $dbver);
+		}
+	}
+
+function bit_update_db_check() {
+    if ( get_site_option( 'bit_dbver' ) != BIT_DB_VERSION ) {
+        bit_create_tables();
+        //bit_create_mediatables();
+    }
 }
 
-register_activation_hook( __FILE__, 'bit_plugin_activation' );
+add_action( 'plugins_loaded', 'bit_update_db_check' );
+
+register_activation_hook( __FILE__, 'bit_update_db_check' );
 
 function bit_populate_text_tables( $data ) {
 	//upload data to text tables
@@ -75,9 +122,21 @@ function bit_get_line( $lineid ) {
 
 function bit_get_play( $playid ) {
 	//get intro for play
+	global $wpdb;
+	$tablename = BIT_TABLENAME;
+
+	$results = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}{$tablename} WHERE play_asoc = {$playid}", OBJECT);
+
+	return $results;
 }
 
-function bit_get_media( $mediaid ) {
+function bit_get_media( $playid ) {
 	//get media from mediaid
+	global $wpdb;
+	$media_tablename = BIT_MEDIATABLENAME;
+
+	$results = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}{$media_tablename} WHERE play_asoc = {$playid}", OBJECT);
+
+	return $results;
 }
 
